85ede4c5a848912362d93bff76f3efb521d13f13
Nimira43 <n.rabone72@aol.com>
2024-09-08 03:56:23 +0100

    Using FBX loader to load car image and Orbit Controls to move car.

--- main.js
+++ main.js
@@ -1,44 +1,49 @@
 class Game{
-  constructor(){
+  constructor() {
+    this.init()  
+  }
+
+  init() {
     this.scene = new THREE.Scene()
     this.camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
-      1000
+      100
     )
+    this.camera.position.set(1, 1, 5)
 
 		this.renderer = new THREE.WebGLRenderer()
     this.renderer.setSize(window.innerWidth, window.innerHeight)
     document.body.appendChild(this.renderer.domElement)
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
     const light = new THREE.DirectionalLight(0xffffff)
 		light.position.set(0, 20, 10)
     const ambient = new THREE.AmbientLight(0x707070)
-    const material = new THREE.MeshPhongMaterial({
-      color: 0x00aaff
-    })
-		
-    this.cube = new THREE.Mesh(geometry, material)
-
-    this.scene.add(this.cube)
+        
+    this.controls = new THREE.OrbitControls(this.camera)
+    
     this.scene.add(light)
     this.scene.add(ambient)
 
-		this.camera.position.z = 3
-    this.animate()
+		const loader = new THREE.FBXLoader()
+    const game = this
+
+    loader.load('./fbx/car.fbx', (object) => {
+      game.car = object
+      game.scene.add(object)
+      game.controls.target = object.position.clone()
+      game.controls.update()
+      object.traverse((child) => {
+        if (child.isMesh) child.castShadow = child.receiveShadow = true
+      })
+      game.animate()
+    }, null, (error) => console.log(error))
   }
     
 	animate() {
     const game = this
-    requestAnimationFrame(function () {
-      game.animate()
-    })
-
-    this.cube.rotation.x += 0.01
-    this.cube.rotation.y += 0.01
-
+    requestAnimationFrame(() => game.animate())
     this.renderer.render(this.scene, this.camera)
   }
 }
